23cabc8c5bf1f7e14b3385a6bfcc3b1c6c253fd4
Chris Phillipson <cphillipson@pivotal.io>
2019-07-15 17:43:21 -0700

    ConfigurationProperties is Configuration * remove @Configuration annotation

--- src/main/java/io/pivotal/cfapp/config/HooverSettings.java
+++ src/main/java/io/pivotal/cfapp/config/HooverSettings.java
@@ -5,13 +5,11 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
 
 @Data
 @RefreshScope
-@Configuration
 @ConfigurationProperties(prefix = "cf")
 public class HooverSettings {
 
