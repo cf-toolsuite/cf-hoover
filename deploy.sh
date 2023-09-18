110be4cdb8db9ef552788fac68e19881d51284bc
Chris Phillipson <cphillipson@pivotal.io>
2023-09-17 17:14:33 -0700

    Fix deploy scripts, add loop-check, cf bind-service when ready

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,9 @@ export APP_NAME=cf-hoover
 
 cf push --no-start
 cf create-service p.config-server standard $APP_NAME-config -c config/config-server.json
+while [[ $(cf service $APP_NAME-config) != *"succeeded"* ]]; do
+  echo "$APP_NAME-config is not ready yet..."
+  sleep 5s
+done
 cf bind-service $APP_NAME $APP_NAME-config
 cf start $APP_NAME
--- deploy.with-registry.sh
+++ deploy.with-registry.sh
@@ -8,7 +8,14 @@ export REGISTRY_NAME=hooverRegistry
 cf push -f manifest.with-registry.yml --no-start
 cf create-service p.config-server standard $APP_NAME-config -c config/config-server.json
 cf create-service p.service-registry standard $REGISTRY_NAME
-sleep 10m
+while [[ $(cf service $APP_NAME-config) != *"succeeded"* ]]; do
+  echo "$APP_NAME-config is not ready yet..."
+  sleep 5s
+done
 cf bind-service $APP_NAME $APP_NAME-config
+while [[ $(cf service $REGISTRY_NAME) != *"succeeded"* ]]; do
+  echo "$REGISTRY_NAME is not ready yet..."
+  sleep 5s
+done
 cf bind-service $APP_NAME $REGISTRY_NAME
 cf start $APP_NAME
