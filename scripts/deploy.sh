192dbae63b7be4691086fbc533684ffbe8a76d52
Shawn Neal <sneal@sneal.net>
2024-04-08 21:38:22 -0700

    Add Mac compatible sleep (#415)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cf push --no-start
 cf create-service p.config-server standard $APP_NAME-config -c config/config-server.json
 while [[ $(cf service $APP_NAME-config) != *"succeeded"* ]]; do
   echo "$APP_NAME-config is not ready yet..."
-  sleep 5s
+  sleep 5
 done
 cf bind-service $APP_NAME $APP_NAME-config
 cf start $APP_NAME
