344a8d6c6f0f620a2dd9f28553be5272cd0be3b5
Chris Phillipson <cphillipson@pivotal.io>
2024-04-13 10:10:09 -0700

    Faciliate accepting additional push options on deploy

--- scripts/deploy.with-registry.sh
+++ scripts/deploy.with-registry.sh
@@ -5,17 +5,25 @@ set -e
 export APP_NAME=cf-hoover
 export REGISTRY_NAME=hooverRegistry
 
-cf push -f manifest.with-registry.yml --no-start
+cf push -f manifest.with-registry.yml --no-start "$1"
 cf create-service p.config-server standard $APP_NAME-config -c config/config-server.json
 cf create-service p.service-registry standard $REGISTRY_NAME
-while [[ $(cf service $APP_NAME-config) != *"succeeded"* ]]; do
-  echo "$APP_NAME-config is not ready yet..."
-  sleep 5
+for (( i = 0; i < 90; i++ )); do
+  if [[ $(cf service $APP_NAME-config) != *"succeeded"* ]]; then
+    echo "$APP_NAME-config is not ready yet..."
+    sleep 10
+  else
+    break
+  fi
 done
 cf bind-service $APP_NAME $APP_NAME-config
-while [[ $(cf service $REGISTRY_NAME) != *"succeeded"* ]]; do
-  echo "$REGISTRY_NAME is not ready yet..."
-  sleep 5
+for (( i = 0; i < 90; i++ )); do
+  if [[ $(cf service $REGISTRY_NAME) != *"succeeded"* ]]; then
+    echo "$REGISTRY_NAME is not ready yet..."
+    sleep 10
+  else
+    break
+  fi
 done
 cf bind-service $APP_NAME $REGISTRY_NAME
 cf start $APP_NAME
