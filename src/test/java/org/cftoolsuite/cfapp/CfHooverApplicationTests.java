816c25cf2b8d38c9bb234304337c2c3551e30d6a
Chris Phillipson <cphillipson@pivotal.io>
2025-01-16 11:58:31 -0800

    Fix test failure. Add test property source.

--- src/test/java/org/cftoolsuite/cfapp/CfHooverApplicationTests.java
+++ src/test/java/org/cftoolsuite/cfapp/CfHooverApplicationTests.java
@@ -2,8 +2,10 @@ package org.cftoolsuite.cfapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "cf.butlers.example=https://cf-butler.example.com")
 public class CfHooverApplicationTests {
 
 	@Test
